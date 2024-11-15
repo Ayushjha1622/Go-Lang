95c32294382ab2b035806bcc3dac2b1c56688141
Ayush jha <jhaa97204@gmail.com>
2024-11-15 14:10:19 +0530

    new file

--- 08_Array/main.go
+++ 08_Array/main.go
@@ -1,5 +1,21 @@
 package main
 
+import(
+	"fmt"
+)
 func main(){
-	
+	fmt.Println("welcome to array in golang")
+
+	var fruitList [4]string
+	fruitList[0] = "apple"
+	fruitList[1] = "banana"
+	fruitList[2] = "cherry"
+	fruitList[3] = "date"
+
+	fmt.Println("fruit List is: ", fruitList)
+	fmt.Println("fruit List is: ", len(fruitList))
+
+
+	var vegList = [4]string{"potato","beans","tomato","gobhi"}
+	fmt.Println(vegList)
 }
